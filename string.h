39e973ecf123c3a43a818cfcb7aab91ee06a25ef
shoham1246 <shoham.marom@campus.technion.ac.il>
2025-01-25 18:34:04 +0200

    Update string.h sho 25.1

--- string.h
+++ string.h
@@ -1,17 +1,29 @@
 
+#ifndef STRING_H
+#define STRING_H
+
 #include "generic-string.h"
 #include "string-array.h"
 
 
-class String;
-
-
-StringArray split(const char *delimiters)
-GenericString& operator=(const char *str)
-GenericString& trim()
-bool operator==(const GenericString &other)
-bool operator==(const char *other)
-int to_integer()
-String& as_string()
-const String& as_string()
+class String : public GenericString {
+ public :
+	char* strg; //mustnt be const oherwise cant change!! and probably wasnt allowed to use std::string
+ public :
+ 	String(const char* str); //constructor
+ 	String(const String& other); //copy constructor
+	StringArray split(const char *delimiters) const;
+	GenericString& operator=(const char *str);
+	GenericString& trim();
+	bool operator==(const GenericString &other) const;
+	bool operator==(const char *other) const;
+	int to_integer() const;
+	String& as_string();
+	const String& as_string() const;
+	GenericString* make_string(const char *str);
+	void printMethod();
+	~String();
+};
 GenericString* make_string(const char *str);
+
+#endif
